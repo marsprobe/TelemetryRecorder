b87a4756db3d647eea4f393c0830fdd76a0ac992
Misu <mciuraru@gmail.com>
2017-02-18 20:27:01 +0200

    Added NTP support

--- src/TelemetryRecorder.ino
+++ src/TelemetryRecorder.ino
@@ -2,9 +2,10 @@
 #include <MPU9250.h>
 #include <WiFiUdp.h>
 #include <ESP8266WiFi.h>
-#include <SPI.h>
 #include <SD.h>
 #include <config.h>
+#include <TimeLib.h>
+
 
 // Pin definitions
 const int intPin = D6;  // These can be changed, 2 and 3 are the Arduinos ext int pins
@@ -20,12 +21,16 @@ bool ledState = 0;
 int wifiRetry;
 String stringOne;
 char charBuf[96];
+IPAddress timeServerIP;
+const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
+byte packetBuffer[ NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
 
 //Configuration parameters:
 const IPAddress IP_Remote(172, 16, 0, 10); //IP address to stream data to
+const char* ntpServerName = "time.nist.gov"; //NTP time server
 const unsigned int localUdpPort = 4210; //UDP port to stream data
 const bool stream = 1; //if set to 1 start streaming UDP to IP_Remote
-const bool logToSD = 1; //if set to 1 start logging data to SD card
+const bool logToSD = 0; //if set to 1 start logging data to SD card
 #define SerialDebug true  // Set to true to get Serial output for debugging
 
 //Initialize libraries
@@ -51,8 +56,14 @@ void setup()
   digitalWrite(outLed, HIGH);
 
   startMPU9250();
+  if(logToSD){
+    initSDcard();
+  }
 
-  initSDcard();
+  queryNTP ();
+  Serial.print("System time is: ");
+  Serial.println(now());
+
 
 }
 
@@ -60,7 +71,7 @@ void loop()
 {
   getMPU9250values();
 
-  stringOne = String(millis()) + ",";
+  stringOne = String(now()) + ",";
   stringOne = stringOne + String((int)1000*myIMU.ax) + "," + String((int)1000*myIMU.ay) + "," + String((int)1000*myIMU.az) + ",";
   stringOne = stringOne + String(myIMU.gx) + "," + String(myIMU.gy) + "," + String(myIMU.gz) + ",";
   stringOne = stringOne + String(myIMU.mx) + "," + String(myIMU.my) + "," + String(myIMU.mz);
@@ -80,6 +91,9 @@ void loop()
       }
     lastLogTime = millis();
     toggleLed();
+    if(SerialDebug) {
+      Serial.println(charBuf);
+    }
   }
 }
 
@@ -270,7 +284,7 @@ void getMPU9250values() {
       Serial.print(myIMU.roll, 2);
       Serial.print("  ");
       Serial.println((float)myIMU.sumCount/myIMU.sum, 2);
-      Serial.printf("settings heap size: %u\n", ESP.getFreeHeap());
+      Serial.printf("Settings heap size: %u\n", ESP.getFreeHeap());
     }
     myIMU.yaw   = atan2(2.0f * (*(getQ()+1) * *(getQ()+2) + *getQ() *
                   *(getQ()+3)), *getQ() * *getQ() + *(getQ()+1) * *(getQ()+1)
@@ -377,4 +391,98 @@ void initSDcard() {
       }
       Serial.println("card initialized.");
     }
+}
+
+// send an NTP request to the time server at the given address
+unsigned long sendNTPpacket(IPAddress& address)
+{
+  Serial.println("sending NTP packet...");
+  // set all bytes in the buffer to 0
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12]  = 49;
+  packetBuffer[13]  = 0x4E;
+  packetBuffer[14]  = 49;
+  packetBuffer[15]  = 52;
+
+  // all NTP fields have been given values, now
+  // you can send a packet requesting a timestamp:
+  Udp.beginPacket(address, 123); //NTP requests are to port 123
+  Udp.write(packetBuffer, NTP_PACKET_SIZE);
+  Udp.endPacket();
+}
+
+void queryNTP (){
+  //get a random server from the pool
+  WiFi.hostByName(ntpServerName, timeServerIP);
+
+  Serial.print("Resolving: ");
+  Serial.print(ntpServerName);
+  Serial.print(" -> ");
+  Serial.println(timeServerIP);
+
+  sendNTPpacket(timeServerIP); // send an NTP packet to a time server
+  // wait to see if a reply is available
+  delay(1000);
+  int cb;
+  int ntpRetries = 5;
+  while (!cb && ntpRetries > 0) {
+    Serial.print("no packet yet ");
+    Serial.println(ntpRetries);
+    cb = Udp.parsePacket();
+    delay(1000);
+    ntpRetries--;
+  }
+  if(cb) {
+    Serial.print("packet received, length=");
+    Serial.println(cb);
+    // We've received a packet, read the data from it
+    Udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
+
+    //the timestamp starts at byte 40 of the received packet and is four bytes,
+    // or two words, long. First, esxtract the two words:
+
+    unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
+    unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
+    // combine the four bytes (two words) into a long integer
+    // this is NTP time (seconds since Jan 1 1900):
+    unsigned long secsSince1900 = highWord << 16 | lowWord;
+    Serial.print("Seconds since Jan 1 1900 = " );
+    Serial.println(secsSince1900);
+
+    // now convert NTP time into everyday time:
+    Serial.print("Unix time = ");
+    // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
+    const unsigned long seventyYears = 2208988800UL;
+    // subtract seventy years:
+    unsigned long epoch = secsSince1900 - seventyYears;
+    // print Unix time:
+    Serial.println(epoch);
+
+
+    // print the hour, minute and second:
+    Serial.print("The UTC time is ");       // UTC is the time at Greenwich Meridian (GMT)
+    Serial.print((epoch  % 86400L) / 3600); // print the hour (86400 equals secs per day)
+    Serial.print(':');
+    if ( ((epoch % 3600) / 60) < 10 ) {
+      // In the first 10 minutes of each hour, we'll want a leading '0'
+      Serial.print('0');
+    }
+    Serial.print((epoch  % 3600) / 60); // print the minute (3600 equals secs per minute)
+    Serial.print(':');
+    if ( (epoch % 60) < 10 ) {
+      // In the first 10 seconds of each minute, we'll want a leading '0'
+      Serial.print('0');
+    }
+    Serial.println(epoch % 60); // print the second
+
+    //Set system time
+    setTime(epoch);
+  }
 }
